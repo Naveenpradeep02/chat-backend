869bd62d2d030084de2cb3d7108df198d937af2a
naveenpradeep0987@gmail.com <Naveenpradeep02.noreply.github.com>
2025-03-24 17:22:06 +0530

    update

--- src/index.js
+++ src/index.js
@@ -24,13 +24,13 @@ app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+//   });
+// }
 
 server.listen(Port, () => {
   console.log(`server is running on port ${Port}`);
